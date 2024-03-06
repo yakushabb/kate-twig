8fa708129511ec69a048d1e5ec97cd405392716a
Sabri Ünal <161761531+yakushabb@users.noreply.github.com>
2024-03-06 10:17:19 +0300

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-dirname="$(kde4-config --localprefix)share/apps/katepart/syntax/"
-if [ -d "$dirname"  ]; then
-	cp -vi twig.xml "$dirname";
-else
-	echo "$dirname doesn't exist";
-fi
